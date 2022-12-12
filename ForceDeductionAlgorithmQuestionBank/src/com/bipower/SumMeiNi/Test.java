4a2554dacbbd54bb3a9a3788835ce22a4cf31893
Starsstares <2640274295@qq.com>
2022-12-12 17:35:43 +0800

    1781. 所有子字符串美丽值之和
    这是力扣一道算法题
    1781. 所有子字符串美丽值之和
    注释,代码都已实现,可用idea打开

---
+++ ForceDeductionAlgorithmQuestionBank/src/com/bipower/SumMeiNi/Test.java
@@ -0,0 +1,10 @@
+package com.bipower.SumMeiNi;
+
+public class Test {
+    public static void main(String[] args) {
+        Solution solution=new Solution();
+
+        System.out.println(solution.beautySum("abaacc"));
+
+    }
+}
